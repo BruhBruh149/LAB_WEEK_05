4ac6fca0f7f8cf3ddb86e0287380673ce6e54af0
Haikal Ibrahim Fathurrahman <90713429+BruhBruh149@users.noreply.github.com>
2025-09-23 00:18:30 +0700

    Commit No. 4 - Assignment nambah ss commit 1

--- app/src/main/java/com/example/lab_week_05/api/CatApiService.kt
+++ app/src/main/java/com/example/lab_week_05/api/CatApiService.kt
@@ -10,5 +10,5 @@ interface CatApiService {
     fun searchImages(
         @Query("limit") limit: Int,
         @Query("size") format: String
-    ): Call<List<ImageData>>   // Part 2: Moshi-mapped model list
+    ): Call<List<ImageData>>
 }
